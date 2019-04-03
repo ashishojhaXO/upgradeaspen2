dc1ac2fca085c0bac52ac3223cfebd071f962f8d
dyadav123 <dyadav@fusionseven.com>
2019-04-03 10:45:06 -0700

    FIX : date format

--- src/app/shared/components/wrapper-multiselect-dropdown/wrapper-multiselect-dropdown.component.ts
+++ src/app/shared/components/wrapper-multiselect-dropdown/wrapper-multiselect-dropdown.component.ts
@@ -1,5 +1,6 @@
 import {Component, OnInit, Input, Output, EventEmitter} from '@angular/core';
 import {Router, ActivatedRoute} from '@angular/router';
+import {formatDate} from 'ngx-bootstrap/chronos';
 
 @Component({
   selector: 'app-wrapper-multiselect-drop-down',
@@ -44,10 +45,7 @@ export class WrapperMultiSelectDropdownComponent implements OnInit {
     this.loading = false;
     this.data = this.getData();
 
-    var selectedItem = {
-      id: '2019-2-01',
-      itemName: 'Feb 2019'
-    };
+    var selectedItem = this.data[0];
 
     this.filterConfig.values.push(selectedItem);
     this.valueUpdate.emit(this.filterConfig);
@@ -58,8 +56,9 @@ export class WrapperMultiSelectDropdownComponent implements OnInit {
     var monthName = new Array('Jan', 'Feb', 'Mar', 'Apr', 'May', 'June', 'July', 'Aug', 'Sep', 'Oct', 'Nov', 'Dec');
     var d = new Date();
     for (var i=0; i<=12; i++) {
+      var monthVal = (d.getMonth() + 1).toString().length === 1 ? ('0' + (d.getMonth() + 1)) : (d.getMonth() + 1);
       months.push({
-        'id' : d.getFullYear() + '-' + (d.getMonth() + 1) + '-' + '01',
+        'id' : d.getFullYear() + '-' + monthVal + '-' + '01',
         'itemName' : monthName[d.getMonth()] + ' ' + d.getFullYear()
       })
       d.setMonth(d.getMonth() - 1);
@@ -85,5 +84,17 @@ export class WrapperMultiSelectDropdownComponent implements OnInit {
       this.filterConfig.values.splice(this.filterConfig.values.indexOf(item), 1);
     }
     this.valueUpdate.emit(this.filterConfig);
+  }
+
+  formatDate(date) {
+    var d = new Date(date),
+        month = '' + (d.getMonth() + 1),
+        day = '' + d.getDate(),
+        year = d.getFullYear();
+
+    if (month.length < 2) month = '0' + month;
+    if (day.length < 2) day = '0' + day;
+
+    return [year, month, day].join('-');
   }
 }
