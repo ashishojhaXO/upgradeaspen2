709c7bcb45c3534b8490667a1c9ce203aeb7537e
Ashish Ojha <ashish.ojha@xoriant.com>
2019-10-20 20:01:41 +0530

    random

--- src/app/shared/components/app-pop-up-component/app-pop-up-component.component.ts
+++ src/app/shared/components/app-pop-up-component/app-pop-up-component.component.ts
@@ -13,48 +13,4 @@ export class AppPopUpComponentComponent implements OnInit {
   ngOnInit() {
   }
 
-  // PopUpComponent
-  questionPopUp(data: {}, serviceFunc ): {} {
-
-    const startOptions = {
-      title: 'Retry Orders',
-      text: `Are you sure you wish to retry payments of order ids: ${data["ar_id"]}`,
-      type: 'question',
-      showCloseButton: true,
-      showCancelButton: true,
-      cancelButtonText: "Cancel",
-      confirmButtonText: "Submit",
-      reverseButtons: true,
-      showLoaderOnConfirm: true,
-      preConfirm: (login) => {
-        console.log("PRECOONFIRM", login);
-        // return this.postRetryOrderService(data).toPromise();
-        return serviceFunc().toPromise();
-      },
-
-    };
-
-    return startOptions;
-  }
-
-  errorPopUp(options?: {}): {} {
-    const startOptions = {
-      title: "Operation Failed",
-      text: "Operation to retry orders ids failed",
-      type: 'error',
-      // cancelButtonText: "Ok",
-    };
-
-    return startOptions;
-  }
-
-  showPopUp(options) {
-    return Swal.fire(options)
-  }
-
-  runCompileShowPopUp(data, serviceFunc) {
-    const options = this.questionPopUp(data, serviceFunc);
-    return this.showPopUp(options);
-  }
-  // PopUpComponent/
 }
