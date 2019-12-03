8efb38cf295bbd5ba2f4f3e1a2b0dd91f32c00b7
Ashish Ojha <ashish.ojha@xoriant.com>
2019-12-03 19:33:45 +0530

    U: changes in the okta class

--- src/services/okta.service.ts
+++ src/services/okta.service.ts
@@ -1,9 +1,16 @@
 import { Injectable } from '@angular/core';
 import { Observable } from 'rxjs';
 import * as OktaSignIn from '@okta/okta-signin-widget/dist/js/okta-sign-in-no-jquery';
+import { Router, ActivatedRoute } from '@angular/router';
 
 @Injectable()
 export class OktaAuthService {
+
+  constructor(
+    private route: ActivatedRoute, private router: Router
+  ) {
+
+  }
 
   // widget = new OktaSignIn({
   //   logo: 'https://op1static.oktacdn.com/bc/image/fileStoreRecord?id=fs0iw1hhzapgSwb4s0h7', // Try changing "okta.com" to other domains, like: "workday.com", "splunk.com", or "delmonte.com"
@@ -24,12 +31,52 @@ export class OktaAuthService {
   //   }
   // });
 
+  logOutService() {
+    console.log("lOSERV remove all localSTOR")
+    return this.router.navigate(['/loginnew']);
+  }
 
+  signOut() {
+    // TODO: SignOut to be implemented
+
+  }
+
+
+  logOut(accessToken) {
+
+    console.log("acccc: logOUT", accessToken)
+    const self = this;
+
+    return new Promise( function (resolve, reject ) {
+      console.log("inside resolver", self, this)
+      self.logOutService().then( () => {
+        console.log("logOUSERV.then resovl")
+        return resolve(localStorage.getItem('accessToken')) 
+        }
+      )
+    })
+  }
+
+  refresh(accessToken){
+    console.log("acccc: refresh", accessToken)
+    return new Promise( function (resolve, reject ) {
+        () => resolve(localStorage.getItem('accessToken'))
+    })
+  }
+
+  tokenManager = {
+    refresh: (accessToken) => {
+      console.log("refresh: ", accessToken);
+      return this.logOut.apply(this, [accessToken]);
+    }
+  }
 
   widget() {
-    return new Promise( function (resolve, reject ) {
-      setTimeout( () => resolve("done"), 1000);
-    })
+    // return new Promise( function (resolve, reject ) {
+    //   setTimeout( () => resolve("done"), 1000);
+    // })
+
+    return this;
   }
 
 
@@ -37,7 +84,7 @@ export class OktaAuthService {
 
   getWidget() {
     console.log("getWidget: ")
-    return this.widget;
+    return this.widget();
     // return 
   }
 
