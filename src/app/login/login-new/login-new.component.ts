76eb523c370dc9731c8e40b6d45674de82cc6ffa
dyadav123 <dyadav@fusionseven.com>
2019-12-05 15:16:48 -0800

    FIX : role object check

--- src/app/login/login-new/login-new.component.ts
+++ src/app/login/login-new/login-new.component.ts
@@ -91,7 +91,7 @@ export class LoginNewComponent implements OnInit {
     localStorage.setItem('idToken', res.body.id_token);
     localStorage.setItem('loggedInUserName', res.body.first_name.trim() + " " + res.body.last_name.trim());
     localStorage.setItem('loggedInUserID', res.body.external_id);
-    localStorage.setItem('loggedInUserGroup', JSON.stringify([res.body.user_role.name.toUpperCase()] ) );
+    localStorage.setItem('loggedInUserGroup', JSON.stringify([res.body.user_role ? res.body.user_role.name.toUpperCase() : '']));
     localStorage.setItem('loggedInOrg', res.body.org && res.body.org.org_name ? res.body.org.org_name : 'Home Depot');
   }
 
